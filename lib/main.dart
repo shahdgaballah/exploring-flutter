8f86758f249681dcfbf71638d00e21fdc415aa6a
shahdgaballah <shahdgabllah@gmail.com>
2025-04-02 22:35:10 +0200

    fourth commit: login form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/home_screen.dart';
+import 'package:my_app/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
